2fe3d306cfedc7b0d9cdd4e02f5a935ea6a6e338
aripalo <ari@aripalo.fi>
2014-04-20 04:01:12 +0300

    list new methods in help.js

--- modules/help.js
+++ modules/help.js
@@ -16,8 +16,19 @@ module.exports = {
 
     this.string = 'The available commands are: ';//reset the string
     this.string += '\n!help (this command you just ran)';
-    this.string += '!reload (admin only)';
-    this.string += ', !quit (admin only)';
+    this.string += '!reload';
+    this.string += ', !quit';
+    this.string += ', !join';
+    this.string += ', !part';
+    this.string += ', !say';
+    this.string += ', !kick';
+    this.string += ', !ban';
+    this.string += ', !unban';
+    this.string += ', !topic';
+    this.string += ', !op';
+    this.string += ', !deop';
+    this.string += ', !mode';
+
 
     fs.readdirSync('./commands/').forEach(function (file) {
       this.string += ', !'+file.replace(/\.js$/, '');
